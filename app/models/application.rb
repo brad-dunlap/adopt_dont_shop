e17638c67e2643e518b3d39b58131d23c07825b7
Bobby Luly <bobbyluly@gmail.com>
2023-02-10 16:50:23 -0700

    Feat: Add functionality to prevent empty form fields, and add validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,9 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 	attribute :status, :string, default: 'In Progress'
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true 
+  validates :zip_code, presence: true, numericality: true
 end
