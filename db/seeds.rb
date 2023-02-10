94fa62f034ad9dce751632a2638f014e90552139
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-10 16:18:36 -0500

    feat: can link pets to application. User story 1 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Application.destroy_all
+PetApplication.destroy_all
 Pet.destroy_all
+Shelter.destroy_all
 
-Application.create!(name: 'Bobby Lully', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 12345, description: "I need a friend", status: "Pending")
+application = Application.create!(name: 'Bobby Lully', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 12345, description: "I need a friend", status: "Pending")
+shelter1 = Shelter.create!(foster_program: false, name: "The Farm", city: "Denver", rank: 2)
+scooby = shelter1.pets.create!(adoptable: true, age: 1, breed: "Great Dane", shelter_id: 1, name: 'Scooby', )
+pet_application = PetApplication.create!(pet: scooby, application: application)
 
-Shelter.create!(foster_program: false, name: "The Farm", city: "Denver", rank: 2)
 
-Pet.create!(adoptable: true, age: 1, breed: "Great Dane", shelter_id: 1, name: 'Scooby', )
