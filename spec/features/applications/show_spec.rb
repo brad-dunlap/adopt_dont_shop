f6a2f2a646b07cab834fe35be433623aedc311f2
Bobby Luly <bobbyluly@gmail.com>
2023-02-09 17:00:03 -0700

    Change location of Applications show page to a new applications controller

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the application show' do
   it "shows the application and all it's attributes" do
     application = Application.create!(name: 'Bobby Lully', address: '123 Sesame Street, Denver, CO 12345', description: "I need a friend", pets: "Scooby", status: "Pending")
 
-    visit "/#{application.id}"
+    visit "/applications/#{application.id}"
 
     expect(page).to have_content(application.pets)
     expect(page).to have_content(application.name)
