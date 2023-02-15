e49f6d8cac397253b9df6c6ae0c2fcc72b24e3fb
Bobby Luly <bobbyluly@gmail.com>
2023-02-14 19:37:18 -0700

    Refactor: change application status to an enum. Change all files to reflect the change. Add a pet_status enum attribute to PetApplication migration

--- spec/features/admin_applications/show_spec.rb
+++ spec/features/admin_applications/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Admin application show' do
   it "has a button to approve each pet, which disappears after being clicked, confirmation message" do
     shelter_1 = Shelter.create!(foster_program: true, name: 'Fuzzy Friends', city: 'Denver', rank: 1)
     pickles = shelter_1.pets.create!(adoptable: true, age: 2, breed: 'Domestic Shorthair', name: 'Ms. Pickles')
-    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: "Pending")
+    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: 1)
     PetApplication.create!(pet: pickles, application: app_1)
 
     visit "/admin/applications/#{app_1.id}"
@@ -21,7 +21,7 @@ RSpec.describe 'Admin application show' do
   it "has a button to reject each pet, which disappears after being clicked, confirmation message" do
     shelter_1 = Shelter.create!(foster_program: true, name: 'Fuzzy Friends', city: 'Denver', rank: 1)
     pickles = shelter_1.pets.create!(adoptable: true, age: 2, breed: 'Domestic Shorthair', name: 'Ms. Pickles')
-    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: "Pending")
+    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: 1)
     PetApplication.create!(pet: pickles, application: app_1)
 
     visit "/admin/applications/#{app_1.id}"
@@ -38,8 +38,8 @@ RSpec.describe 'Admin application show' do
   it "tests that each application can accept/reject pets independently of one another" do
     shelter_1 = Shelter.create!(foster_program: true, name: 'Fuzzy Friends', city: 'Denver', rank: 1)
     pickles = shelter_1.pets.create!(adoptable: true, age: 2, breed: 'Domestic Shorthair', name: 'Ms. Pickles')
-    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: "Pending")
-    app_2 = Application.create!(name: 'Brad Dunlap', street_address: '124 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: "Pending")
+    app_1 = Application.create!(name: 'Bobby Luly', street_address: '123 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: 1)
+    app_2 = Application.create!(name: 'Brad Dunlap', street_address: '124 Sesame Street', city: 'Denver', state: 'CO', zip_code: 80123, status: 1)
     PetApplication.create!(pet: pickles, application: app_1)
     PetApplication.create!(pet: pickles, application: app_2)
 
