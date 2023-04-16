dadeb4378f5071bc60c9492d9a0002153e0c457c
Brad Dunlap <braddunlapdpt@gmail.com>
2023-04-16 14:27:10 -0600

    updated app to rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_041753) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_202229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,16 +22,16 @@ ActiveRecord::Schema.define(version: 2023_02_10_041753) do
     t.integer "zip_code"
     t.string "description"
     t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "pet_applications", force: :cascade do |t|
     t.integer "pet_status", default: 0
     t.bigint "pet_id"
     t.bigint "application_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
@@ -43,8 +42,8 @@ ActiveRecord::Schema.define(version: 2023_02_10_041753) do
     t.string "breed"
     t.string "name"
     t.bigint "shelter_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
@@ -53,8 +52,8 @@ ActiveRecord::Schema.define(version: 2023_02_10_041753) do
     t.string "name"
     t.string "city"
     t.integer "rank"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "veterinarians", force: :cascade do |t|
@@ -62,8 +61,8 @@ ActiveRecord::Schema.define(version: 2023_02_10_041753) do
     t.integer "review_rating"
     t.string "name"
     t.bigint "veterinary_office_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["veterinary_office_id"], name: "index_veterinarians_on_veterinary_office_id"
   end
 
@@ -71,8 +70,8 @@ ActiveRecord::Schema.define(version: 2023_02_10_041753) do
     t.boolean "boarding_services"
     t.integer "max_patient_capacity"
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   add_foreign_key "pet_applications", "applications"
